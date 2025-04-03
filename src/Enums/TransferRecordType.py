e7249fccb1484e63cbe67e67daf2e1610dd952e0
alex.chen <alex.chen@25sprout.com>
2025-04-03 14:59:11 +0800

    重構轉帳紀錄類型，將類別名稱更改為轉帳原因類型，並新增多種轉帳原因選項

--- src/Enums/TransferRecordType.py
+++ src/Enums/TransferRecordType.py
@@ -1,7 +1,22 @@
 from enum import Enum
-class TransferRecordType(Enum):
+class TransferReasonType(Enum):
+    # 玩家彼此轉帳
     TRANSFER = 'TRANSFER'
+
+    # 系統發放金錢
     GIVE = 'GIVE'
+
+    # 系統扣除金錢
     TAKE = 'TAKE'
+
+    # 購買商品
     MERCHANDISE = 'MERCHANDISE'
+
+    # 簽到獎勵發放
     CHECK_IN = 'CHECK_IN'
+
+    # 退款
+    REFUND = 'REFUND'
+
+    # 消費者兌換商品後得到金錢
+    REDEEM = 'REDEEM'
