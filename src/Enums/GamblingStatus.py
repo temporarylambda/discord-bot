f3dc676d16118cb12ae7afaa424d734b81102586
alex.chen <alex.chen@25sprout.com>
2025-04-16 23:29:48 +0800

    feat: 移除賭局相關的類別與檔案，包括賭局狀態、賭局類型、賭局異常及賭客資料庫操作

--- src/Enums/GamblingStatus.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-class GamblingStatus(Enum):
-    PENDING  = 'PENDING'    # 待開始
-    GAMBLING = 'GAMBLING'   # 進行中
-    CANCELED = 'CANCELED'   # 已取消
-    FINISHED = 'FINISHED'   # 已結束
--- src/Enums/GamblingType.py
+++
@@ -1,3 +0,0 @@
-from enum import Enum
-class GamblingType(Enum):
-    DICES_EIGHTEEN  = 'DICES_EIGHTEEN'    # 十八仔
